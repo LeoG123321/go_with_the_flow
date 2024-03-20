b9f0ffea938d595417afe20641a6aac706c9c242
LeoG123321 <3545gonzalez@gmail.com>
2024-03-19 20:33:57 -0500

    fininshed declaring functions for HeatFlow Class now to make sure the code works

--- go_with_the_flow/HeatFlow.cpp
+++ go_with_the_flow/HeatFlow.cpp
@@ -15,9 +15,27 @@ HeatFlow::HeatFlow(double initial_temperature, int number_of_sections, double K,
 void HeatFlow::tick() {
 	vector<double> ghost_bar = bar;
 	ghost_bar.push_back(ghost_bar[ghost_bar.size()]);
+	bool source_flag = false;
 
 	for (int i = 0; i < bar.size(); i++) {
+		for (int j = 0; j < sources.size(); j++) {	//checks if the current position of the bar, i , is a source, sets flag to true.
+			if (i == sources[j].get_source_location())
+				source_flag = true;
+			j = sources.size();
+		}
+		if (!source_flag) {
+			if (i == 0) {
+				bar[i] = ghost_bar[i] + K * (ghost_bar[i + 1] - 2 * ghost_bar[i] + ghost_bar[i]);
+			}
+			else if (i == bar.size() - 1) {
+				bar[i] = ghost_bar[i] + K * (ghost_bar[i] - 2 * ghost_bar[i] + ghost_bar[i - 1]);
+			}
+			else {
+				bar[i] = ghost_bar[i] + K * (ghost_bar[i + 1] - 2 * ghost_bar[i] + ghost_bar[i - 1]);
 
+			}
+		}
+		source_flag = false;
 	}
 }
 
