7d2012bcd08720d6693cd1745de2d28f599c384f
LeoG123321 <3545gonzalez@gmail.com>
2024-03-21 15:18:34 -0500

    attempting to test program program not working

--- go_with_the_flow/go_with_the_flow.cpp
+++ go_with_the_flow/go_with_the_flow.cpp
@@ -5,10 +5,35 @@
 */
 
 #include <iostream>
+#include "HeatFlow.h"
 
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    vector<Source> source_list;
+    Source source1(0, 100);
+    source_list.push_back(source1);
+    string input = "";
+    HeatFlow metal_bar (10.0, 8, 1.0, source_list);
+
+    while (input != "n") {
+        try_again:
+        cout << "Type 'y' to tick time foward"
+            << endl
+            << "Type 'n' to end program"
+            << endl;
+
+        cin >> input;
+
+        if (input == "y") {
+            metal_bar.tick();
+            metal_bar.pretty_print();
+        }
+        else if (input != "n") {
+            cout << "Error: Wrong Input" << endl << endl;
+            goto try_again;
+        }
+    }
 }
 
